ee3481c1c0469db6f2111e83856646a19134c5de
Eduar <eduarnossa315@gmail.com>
2025-08-04 21:24:10 -0500

    Commit

--- screenmatch/src/main/java/com/aluracursos/screenmatch/modelos/Titulo.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -1,5 +1,8 @@
 package com.aluracursos.screenmatch.modelos;
 
+import com.aluracursos.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
     private String nombre;
     private int fechaDeLanzamiento;
@@ -11,6 +14,16 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nombre, int fechaDeLanzamiento) {
         this.nombre = nombre;
         this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public Titulo(TituloOmdb miTituloOmdb) {
+        this.nombre = miTituloOmdb.title();
+        this.fechaDeLanzamiento = Integer.valueOf(miTituloOmdb.year());
+        if (miTituloOmdb.runtime().contains("N/A")){
+            throw new ErrorEnConversionDeDuracionException("No pude convertir la duracion," +
+                    "porque contiene un N/A");
+        }
+        this.duracionEnMinutos = Integer.valueOf(miTituloOmdb.runtime().substring(0,2).replace(" ", ""));
     }
 
     public String getNombre() {
@@ -65,5 +78,13 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return "(nombre='" + nombre +
+                ", fechaDeLanzamiento=" + fechaDeLanzamiento +
+                ", duracion="+duracionEnMinutos+")";
+
     }
 }
---
+++ starwarsapi/src/main/java/Pelicula.java
@@ -0,0 +1,4 @@
+public record Pelicula(String title, int episode_id, String opening_crawl,
+                       String director, String produce, String release_date){
+
+}
